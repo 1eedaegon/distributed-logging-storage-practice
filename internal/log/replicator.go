e1750222f001fec0d9122384b13234d647f865dc
1eedaegon <d8726243@gmail.com>
2023-09-25 20:57:41 +0900

    feat: Add replicator with membership

--- internal/log/replicator.go
+++ internal/log/replicator.go
@@ -2,6 +2,7 @@
 package log
 
 import (
+	"context"
 	"sync"
 
 	api "github.com/1eedaegon/distributed-logging-storage-practice/api/v1"
@@ -17,4 +18,108 @@ type Replicator struct {
 	servers     map[string]chan struct{}
 	closed      bool
 	close       chan struct{}
+}
+
+func (r *Replicator) Join(name, addr string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.init()
+
+	if r.closed {
+		return nil
+	}
+	if _, ok := r.servers[name]; ok {
+		return nil
+	}
+	r.servers[name] = make(chan struct{})
+	go r.replicate(addr, r.servers[name])
+	return nil
+}
+
+func (r *Replicator) replicate(addr string, leave chan struct{}) {
+	// Setup grpc
+	cc, err := grpc.Dial(addr, r.DialOptions...)
+	if err != nil {
+		r.logError(err, "failed to dial", addr)
+		return
+	}
+	defer cc.Close()
+	// Construct grpc client
+	client := api.NewLogClient(cc)
+	ctx := context.Background()
+	// make stream
+	stream, err := client.ConsumeStream(ctx, &api.ConsumeRequest{Offset: 0})
+	if err != nil {
+		r.logError(err, "failed to consume", addr)
+		return
+	}
+	// make stream channel
+	records := make(chan *api.Record)
+	// PUB/SUB
+	// goroutine stream receiver
+	go func() {
+		for {
+			recv, err := stream.Recv()
+			if err != nil {
+				r.logError(err, "failed to receive", addr)
+				return
+			}
+			records <- recv.Record
+		}
+	}()
+	// stream relay from channel to self producing
+	for {
+		select {
+		case <-r.close:
+			return
+		case <-leave:
+			return
+		case record := <-records:
+			_, err := r.LocalServer.Produce(ctx, &api.ProduceRequest{Record: record})
+			if err != nil {
+				r.logError(err, "failed to produce", addr)
+				return
+			}
+		}
+	}
+}
+
+func (r *Replicator) Leave(name string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.init()
+	if _, ok := r.servers[name]; !ok {
+		return nil
+	}
+	close(r.servers[name])
+	delete(r.servers, name)
+	return nil
+}
+
+func (r *Replicator) Close() error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.init()
+	if r.closed {
+		return nil
+	}
+	r.closed = true
+	close(r.close)
+	return nil
+}
+
+func (r *Replicator) init() {
+	if r.logger == nil {
+		r.logger = zap.L().Named("replicator")
+	}
+	if r.servers == nil {
+		r.servers = make(map[string]chan struct{})
+	}
+	if r.close == nil {
+		r.close = make(chan struct{})
+	}
+}
+
+func (r *Replicator) logError(err error, msg, addr string) {
+	r.logger.Error(msg, zap.String("addr", addr), zap.Error(err))
 }
