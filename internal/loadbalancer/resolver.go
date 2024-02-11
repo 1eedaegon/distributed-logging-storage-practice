176bae1c2d749cba565b9e10778ec9c2b1da9cd1
1eedaegon <d8726243@gmail.com>
2024-02-12 00:20:02 +0900

    feat: Add resolver of LB without testing

--- internal/loadbalancer/resolver.go
+++ internal/loadbalancer/resolver.go
@@ -1,12 +1,16 @@
 package loadbalancer
 
 import (
+	"context"
 	"fmt"
 	"sync"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/resolver"
+
+	api "github.com/1eedaegon/distributed-logging-storage-practice/api/v1"
 )
 
 type Resolver struct {
@@ -34,6 +38,50 @@ func (r *Resolver) Build(target resolver.Target, cc resolver.ClientConn, opts re
 	if err != nil {
 		return nil, err
 	}
-	// r.Re
+	r.ResolveNow(resolver.ResolveNowOptions{})
 	return r, nil
+}
+
+const Name = "distributed-logging-storage-practice"
+
+func (r *Resolver) Scheme() string {
+	return Name
+}
+
+func init() {
+	resolver.Register(&Resolver{})
+}
+
+var _ resolver.Resolver = (*Resolver)(nil)
+
+func (r *Resolver) ResolveNow(resolver.ResolveNowOptions) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	client := api.NewLogClient(r.resolverConn)
+	ctx := context.Background()
+	res, err := client.GetServers(ctx, &api.GetServersRequest{})
+
+	if err != nil {
+		r.logger.Error("failed to resolve server", zap.Error(err))
+		return
+	}
+	var addrs []resolver.Address
+	for _, server := range res.Servers {
+		addrs = append(addrs, resolver.Address{
+			Addr:       server.RpcAddr,
+			Attributes: attributes.New("is_leader", server.IsLeader)},
+		)
+	}
+	r.clientConn.UpdateState(resolver.State{
+		Addresses:     addrs,
+		ServiceConfig: r.serviceconfig,
+	})
+
+}
+
+func (r *Resolver) Close() {
+	if err := r.resolverConn.Close(); err != nil {
+		r.logger.Error("failed to close connection", zap.Error(err))
+	}
 }
